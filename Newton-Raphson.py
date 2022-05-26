f0fb772cd35fe4de5e643e59834f31de608c633a
Stevenz10 <stevenzheng2002@gmail.com>
2022-05-25 21:05:52 -0500

    Primera version funcional

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -9,21 +9,21 @@ x = smp.symbols('x', real='True')
 
 f = x**3-2*x-5
 derivada = smp.diff(f,x)
-respuesta = derivada.subs([(x,4)]).evalf()
-print(respuesta)
+
+def calcular(ecuacion,valor):
+      return ecuacion.subs(x,valor).evalf()
 
 # Newton-Raphson Algorithm
 max_iter = 20  # Max iterations
-tol = 1E-15  # Tolerance
+tol = 1  # Tolerance
 i = 0  # Iteration counter
 x0 = 1  # Initial guess
 xi_1 = x0
-print('Iteration ' + str(i) + ': x = ' + str(x0) + ', f(x) = ' + 
-      str(f(x0)))
+
+print('Iteration ' + str(i) + ': x = ' + str(x0) + ', f(x) = ' + str(calcular(f,x0)))
 # Iterating until either the tolerance or max iterations is met
-while abs(f(xi_1)) > tol or i > max_iter:
+while abs(calcular(f,xi_1)) > tol or i > max_iter:
     i = i + 1
-    xi = xi_1-f(xi_1)/smp.diff(f,xi_1)  # Newton-Raphson equation
-    print('Iteration ' + str(i) + ': x = ' + str(xi) + ', f(x) = ' +    
-          str(f(xi)))
+    xi = xi_1-calcular(f,xi_1)/calcular(derivada,xi_1)  # Newton-Raphson equation
+    print('Iteration ' + str(i) + ': x = ' + str(xi) + ', f(x) = ' +    str(calcular(f,xi)))
     xi_1 = xi

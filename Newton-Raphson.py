4687f019dea046b199ab57a118dfcf3ad065110d
Stevenz10 <stevenzheng2002@gmail.com>
2022-05-30 22:39:01 -0500

    Implementacion del input de la ecuacion

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -1,17 +1,10 @@
-from msilib.schema import _Validation_records
 import numpy as np
-import sympy as smp
-import scipy as sp
-from scipy.misc import derivative
 import matplotlib.pyplot as plt
-
-
-x = smp.symbols('x', real='True')
-
-ecuacion = x**3-2*x-5
-derivada = smp.diff(ecuacion,x)
+from sympy import sympify, Symbol
+from sympy.parsing.sympy_parser import parse_expr
 
 def calcular(ecuacion,valor):
+      x = Symbol('x')
       return ecuacion.subs(x,valor).evalf()
 
 
@@ -28,25 +21,36 @@ def graficar(ecuacion):
       plt.plot(valoresX,valoresY)
       plt.show()
       return None
-# Newton-Raphson Algorithm
-max_iter = 20  # Max iterations
-tol = 1  # Tolerance
-i = 0  # Iteration counter
-x0 = 1  # Initial guess
-xi_1 = x0
-
-valores = []
 
 
-print('Iteration ' + str(i) + ': x = ' + str(x0) + ', f(x) = ' + str(calcular(ecuacion,x0)))
-# Iterating until either the tolerance or max iterations is met
-while abs(calcular(ecuacion,xi_1)) > tol or i > max_iter:
-    i = i + 1
-    xi = xi_1-calcular(ecuacion,xi_1)/calcular(derivada,xi_1)  # Newton-Raphson equation
-    valores.append([xi,calcular(ecuacion,xi)])
-    xi_1 = xi
 
-for i,valor in enumerate(valores):
-      print('Iteration ' + str(i) + ': x = ' + str(valor[0]) + ', f(x) = ' +    str(valor[1]))
 
-graficar(ecuacion)
+opcion = 'S'
+
+while opcion == 'S':
+      x = Symbol('x')
+      print("Ejemplo: x**3-2*x-5")
+      ecuacion = parse_expr(input("Introduzca la ecuacion:\n"))
+      derivada = ecuacion.diff(x)
+      # Newton-Raphson Algorithm
+      max_iter = 20  # Max iterations
+      tol = 1  # Tolerance
+      i = 0  # Iteration counter
+      x0 = 1  # Initial guess
+      xi_1 = x0
+
+      valores = []
+
+      print('Iteration ' + str(i) + ': x = ' + str(x0) + ', f(x) = ' + str(calcular(ecuacion,x0)))
+      # Iterating until either the tolerance or max iterations is met
+      while abs(calcular(ecuacion,xi_1)) > tol or i > max_iter:
+            i = i + 1
+            xi = xi_1-calcular(ecuacion,xi_1)/calcular(derivada,xi_1)  # Newton-Raphson equation
+            valores.append([xi,calcular(ecuacion,xi)])
+            xi_1 = xi
+
+      for i,valor in enumerate(valores):
+            print('Iteration ' + str(i) + ': x = ' + str(valor[0]) + ', f(x) = ' +    str(valor[1]))
+      graficar(ecuacion)
+      opcion = input("Desea continuar S/N:  ").upper()
+      
